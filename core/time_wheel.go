02237fe48e71d827a8cc8288f1db542c0e7ed541
houxiangrui <houxiangrui@didiglobal.com>
2021-02-15 21:05:06 +0800

    添加时间间隔最少1ms

--- core/time_wheel.go
+++ core/time_wheel.go
@@ -1,13 +1,16 @@
 package core
 
 import (
+	"fmt"
+	"sync"
 	"time"
 )
+
+const MinIntervalTime = 1
 
 type WheelSlotFunc func(args interface{})
 
 type slotEntity struct {
-	slotId        string
 	args          interface{}
 	wheelSlotFunc WheelSlotFunc
 }
@@ -16,17 +19,30 @@ type wheetSlot []slotEntity
 
 type TimeWheel struct {
 	slotSize         int
+	//min 1ms
 	slotTimeInterval time.Duration
 
 	wheelSlots  []wheetSlot
 	currentSlot int
+
+	lock sync.RWMutex
 }
 
-func (this TimeWheel) InitTimeWheel(slotSize int, slotTimeInterval time.Duration) {
+func (this TimeWheel) Init(slotSize int, slotTimeInterval time.Duration) {
 	this.slotSize = slotSize
+	if slotTimeInterval.Milliseconds() < MinIntervalTime {
+		slotTimeInterval = time.Millisecond
+	}
 	this.slotTimeInterval = slotTimeInterval
 	this.currentSlot = 0
 	this.wheelSlots = make([]wheetSlot, slotSize)
+	this.lock = sync.RWMutex{}
+}
+
+func (this TimeWheel) Start(){
+	for {
+		fmt.Println("test")
+	}
 }
 
 func (this TimeWheel) AddTask(args interface{}, wheelSlotFunc WheelSlotFunc,delayTime time.Duration) {
@@ -35,6 +51,7 @@ func (this TimeWheel) AddTask(args interface{}, wheelSlotFunc WheelSlotFunc,dela
 
 func GetTimeWheel(slotSize int, slotTimeInterval time.Duration) TimeWheel {
 	timeWheel := TimeWheel{}
-	timeWheel.InitTimeWheel(slotSize, slotTimeInterval)
+	timeWheel.Init(slotSize, slotTimeInterval)
+
 	return timeWheel
 }
