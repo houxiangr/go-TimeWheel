a2c4d52af8e222b83ea23a4e4248065266bc07c3
houxiangrui <houxiangrui@didiglobal.com>
2021-02-15 21:53:33 +0800

    get through main flow

--- core/time_wheel.go
+++ core/time_wheel.go
@@ -13,6 +13,14 @@ type WheelSlotFunc func(args interface{})
 type slotEntity struct {
 	args          interface{}
 	wheelSlotFunc WheelSlotFunc
+	//exec be left cycle count
+	loopCount int
+}
+
+func (this *slotEntity)Init(args interface{},wheelSlotFunc WheelSlotFunc,loopCount int){
+	this.loopCount = loopCount
+	this.args = args
+	this.wheelSlotFunc = wheelSlotFunc
 }
 
 type wheetSlot []slotEntity
@@ -28,7 +36,7 @@ type TimeWheel struct {
 	lock sync.RWMutex
 }
 
-func (this TimeWheel) Init(slotSize int, slotTimeInterval time.Duration) {
+func (this *TimeWheel) Init(slotSize int, slotTimeInterval time.Duration) {
 	this.slotSize = slotSize
 	if slotTimeInterval.Milliseconds() < MinIntervalTime {
 		slotTimeInterval = time.Millisecond
@@ -39,14 +47,47 @@ func (this TimeWheel) Init(slotSize int, slotTimeInterval time.Duration) {
 	this.lock = sync.RWMutex{}
 }
 
-func (this TimeWheel) Start(){
+func (this *TimeWheel) Start(){
 	for {
-		fmt.Println("test")
+		fmt.Println("current slot",this.currentSlot)
+		this.lock.RLock()
+		defer this.lock.RUnlock()
+		//exec current slot function
+		slotEntity := this.wheelSlots[this.currentSlot]
+		entityLen := len(slotEntity)
+		for i:=0;i<entityLen;i++ {
+			if slotEntity[i].loopCount > 0 {
+				slotEntity[i].loopCount--
+				continue
+			}
+			slotEntity[i].wheelSlotFunc(slotEntity[i].args)
+			if i == entityLen-1 {
+				slotEntity = slotEntity[:i]
+			} else {
+				slotEntity = append(slotEntity[:i], slotEntity[i+1:]...)
+				i--
+				entityLen--
+			}
+		}
+
+		//update current slot index
+		this.currentSlot = (this.currentSlot+1)%this.slotSize
+
+		//sleep
+		time.Sleep(this.slotTimeInterval)
 	}
 }
 
-func (this TimeWheel) AddTask(args interface{}, wheelSlotFunc WheelSlotFunc,delayTime time.Duration) {
+func (this *TimeWheel) AddTask(args interface{}, wheelSlotFunc WheelSlotFunc,delayTime time.Duration) {
+	loopCount := int(delayTime.Milliseconds()/this.slotTimeInterval.Milliseconds())
+	//structure the insert slot
+	slotEntity := slotEntity{}
+	slotEntity.Init(args,wheelSlotFunc,loopCount/this.slotSize)
 
+	this.lock.Lock()
+	defer this.lock.Unlock()
+	//insert right slot
+	this.wheelSlots[(this.currentSlot + loopCount%this.slotSize)%this.slotSize] = append(this.wheelSlots[(this.currentSlot + loopCount%this.slotSize)%this.slotSize],slotEntity)
 }
 
 func GetTimeWheel(slotSize int, slotTimeInterval time.Duration) TimeWheel {
--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-TimeWheel/core"
 	"time"
 )
 
 func main(){
 	timeWheel := core.GetTimeWheel(10,time.Second)
-	timeWheel.AddTask(1,func(args interface{}){},time.Second)
+	go timeWheel.Start()
+	timeWheel.AddTask(1,func(args interface{}){
+		fmt.Println("test",args)
+	},time.Second*5)
+
+
+	time.Sleep(time.Second*10000)
 }
